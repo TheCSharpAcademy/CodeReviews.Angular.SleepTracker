36cfb4da18755f939b0bb6d2c5571730f58e7d71
Stevie Robinson <stevie.robinson@gmail.com>
2024-02-26 22:47:53 +0100

    build model + db and scaffold API

---
+++ SleepTracker.StevieTV/SleepTrackerAPI/Models/SleepRecord.cs
@@ -0,0 +1,8 @@
+﻿namespace SleepTrackerAPI.Models;
+
+public class SleepRecord
+{
+    public int Id { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+}

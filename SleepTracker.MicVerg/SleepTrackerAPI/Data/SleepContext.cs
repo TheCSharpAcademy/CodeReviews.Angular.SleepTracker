3e74dc27ca7928db469b1122f2f042eeaecaff5c
Michiel Vergauwen <vergauwenmichiel@gmail.com>
2024-05-15 11:43:38 +0200

    Finished API scaffolding

---
+++ SleepTracker.MicVerg/SleepTrackerAPI/Data/SleepContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SleepTrackerAPI.Models;
+
+namespace SleepTrackerAPI.Data
+{
+    public class SleepContext : DbContext
+    {
+        public SleepContext(DbContextOptions<SleepContext> options)
+             : base(options) { }
+
+        public DbSet<Sleep> SleepRecords { get; set; }
+    }
+}

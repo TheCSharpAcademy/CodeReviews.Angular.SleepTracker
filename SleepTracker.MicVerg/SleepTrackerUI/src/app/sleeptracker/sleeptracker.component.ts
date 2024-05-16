3ca7648117cdeb68b11917853924107a290272ff
Michiel Vergauwen <vergauwenmichiel@gmail.com>
2024-05-16 10:09:40 +0200

    fixed paginator

--- SleepTracker.MicVerg/SleepTrackerUI/src/app/sleeptracker/sleeptracker.component.ts
+++ SleepTracker.MicVerg/SleepTrackerUI/src/app/sleeptracker/sleeptracker.component.ts
@@ -1,30 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { SleeptrackerService } from '../sleeptracker.service';
 import { Sleep } from '../sleep.model';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sleeptracker',
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule],
+  imports: [MatTableModule, MatPaginatorModule, MatPaginator, CommonModule],
   templateUrl: './sleeptracker.component.html',
   styleUrl: './sleeptracker.component.css'
 })
-export class SleeptrackerComponent implements OnInit {
-  constructor(private sleepService: SleeptrackerService){}
-  apiUrl: string = 'https://localhost:7240/api/sleeps/';
-  sleepData: Sleep[] = [];
+export class SleeptrackerComponent{
   columnsToDisplay = ['id','startTime','endTime'];
+  dataSource!: MatTableDataSource<Sleep>;
+  sleepData: Sleep[] = [];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  ngOnInit(): void {
+  constructor(private sleepService: SleeptrackerService){
     this.sleepService.getSleepRecords().subscribe((data: Sleep[]) => {
       this.sleepData = data;
-    });
-  }
-
-  getSleeps(){
-    this.sleepService.getSleepRecords()
+      console.log(data);
+      this.dataSource = new MatTableDataSource<Sleep>(this.sleepData);
+      this.dataSource.paginator = this.paginator;
+  })
+}     
+  /* getSleeps(){
+    this.sleepService.getSleepRecords()                               
       .subscribe(sleepData => this.sleepData = sleepData);
-  }
+  } */
 }

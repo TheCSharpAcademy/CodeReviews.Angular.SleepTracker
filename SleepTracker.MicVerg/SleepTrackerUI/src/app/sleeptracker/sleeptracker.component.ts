c5ee28f4538d3dc939487cd70b4045642ac2ea99
Michiel Vergauwen <vergauwenmichiel@gmail.com>
2024-05-16 16:20:05 +0200

    added filter

--- SleepTracker.MicVerg/SleepTrackerUI/src/app/sleeptracker/sleeptracker.component.ts
+++ SleepTracker.MicVerg/SleepTrackerUI/src/app/sleeptracker/sleeptracker.component.ts
@@ -4,11 +4,23 @@ import { SleeptrackerService } from '../sleeptracker.service';
 import { Sleep } from '../sleep.model';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import { CommonModule } from '@angular/common';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatFormField, MatLabel, MatHint } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { MAT_DATE_LOCALE } from "@angular/material/core";
+import { provideMomentDateAdapter } from "@angular/material-moment-adapter";
+import 'moment/locale/en-gb'
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sleeptracker',
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule, MatPaginator, CommonModule],
+  imports: [MatTableModule, MatPaginatorModule, MatPaginator, CommonModule, MatDatepickerModule, MatFormField, MatLabel, MatHint, MatInputModule, FormsModule],
+  providers: [
+    provideMomentDateAdapter(undefined, { useUtc: true }),
+    {provide: MAT_DATE_LOCALE, useValue: 'en-GB'}
+  ],
   templateUrl: './sleeptracker.component.html',
   styleUrl: './sleeptracker.component.css'
 })
@@ -16,6 +28,8 @@ export class SleeptrackerComponent{
   columnsToDisplay = ['id','startTime','endTime', 'duration'];
   dataSource!: MatTableDataSource<Sleep>;
   sleepData: Sleep[] = [];
+  events: string[] = [];
+  selectedDate?: string;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private sleepService: SleeptrackerService){
@@ -25,11 +39,6 @@ export class SleeptrackerComponent{
       this.dataSource.paginator = this.paginator;
   })
 }     
-  /* getSleeps(){
-    this.sleepService.getSleepRecords()                               
-      .subscribe(sleepData => this.sleepData = sleepData);
-  } */
-
   calculateDuration(startTime: string, endTime: string){
     let startDate = new Date(startTime);
     let endDate = new Date(endTime);
@@ -40,4 +49,21 @@ export class SleeptrackerComponent{
     let formattedDuration = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
     return formattedDuration;
   }
+
+  filterSleep(event: MatDatepickerInputEvent<Date>) {
+    if (event.value) {
+      this.selectedDate = event.value.toISOString().substring(0, 10);
+      this.dataSource.filter = this.selectedDate;
+    }
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage()
+    }
+  }
+
+  clearDateFilter() {
+    this.selectedDate = "";
+    this.dataSource.filter = this.selectedDate;
+  }
+
+
 }

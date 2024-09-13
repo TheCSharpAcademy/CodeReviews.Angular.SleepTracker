ec8b992b05398103f5c4d639b22e084d1fc1e5c1
serdar sipahioğlu <sipahioglu_Serdar@yahoo.com>
2024-09-14 01:27:26 +0300

    completed project

---
+++ SleepTracker.Doc415/SleepTrackerAPI/SleepTrackerAPI/Data/SleepContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SleepTrackerAPI.Models;
+namespace SleepTrackerAPI.Data
+{
+    public class SleepContext : DbContext
+    {
+        public SleepContext(DbContextOptions<SleepContext> options) : base(options) { }
+
+        public DbSet<SleepData> SleepLogs { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            var logs = new List<SleepData>();
+            for (int i = 0; i < 20; i++) {
+                logs.Add(
+                        new SleepData()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Date = (DateTime.Now - TimeSpan.FromDays(i)).ToString("yyyy-MM-dd"),
+                            SleepTime = new Random().Next(6000, 19200)
+                        });
+
+            }
+            modelBuilder.Entity<SleepData>().HasData(logs);
+        }
+    }
+}
+  
+
